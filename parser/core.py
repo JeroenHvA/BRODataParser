92193dc260366f5b51794e1d9b8ecfabd8239162
JeroenHvA <165269862+JeroenHvA@users.noreply.github.com>
2025-05-28 09:05:44 +0200

    Added docstrings to core.py, changed naming scheme to be more consistent

--- parser/core.py
+++ parser/core.py
@@ -1,40 +1,57 @@
 # -*- coding: utf-8 -*-
 from typing import Dict, List
 import xml.etree.ElementTree as ET
-from datetime import datetime
 
-from filter import wellFilter, wellFilterData
+from filter import wellFilter
 
 class Well:
 
     def __init__(self, bro_id: str, well_surface_level: float, x_position: float, y_position: float):
         self.bro_id = bro_id
-        self.well_observations: Dict[int, wellFilterData] = {}
+        self.well_filter: Dict[int, wellFilter] = {}
         self.well_surface_level = well_surface_level
         self.x_position = x_position
         self.y_position = y_position
 
-    def add_filters(self, well_number, wellobservation: wellFilterData) -> None:
-        """ Add a well observation to the well."""
-        self.well_observations[well_number] = wellobservation
+    def add_filters(self, well_number, well_filter: wellFilter) -> None:
+        """ This function adds a wellFilter object to the well object.
 
-    def get_filters(self, well_number: int) -> Dict[int, wellFilterData]:
-        if well_number in self.well_observations.keys():
-            return self.well_observations[well_number]
+        Args:
+            well_number (_type_): Number of the filter, used to identify the filter in the well (tubeNumber)
+            well_filter (wellFilter): wellFilter object containing the filter data
+        """
+        self.well_filter[well_number] = well_filter
+
+    def get_filters(self, well_number: int) -> wellFilter:
+        """Get the filter object for a given well number.
+
+        Args:
+            well_number (int): number of the filter in the well (tubeNumber)
+
+        Returns:
+            wellFilter: returns the wellFilter object for the given well number
+        """
+        if well_number in self.well_filter.keys():
+            return self.well_filter[well_number]
         else:
             print(f"well number {well_number} not found in observations.")
             return None
-    def get_well_profile(self) -> List:
-            return {f"{i} --> {v.well_bro_id}": {"top position": v.screen_top_position, 
-                                    "bottom position": v.screen_bottom_position} 
-                                    for i, v in self.well_observations.items()}
+    def get_well_profile(self) -> dict:
+        """Returns a profile of all the filters in the well.
+
+        Returns:
+            dict: Dictionary with the well profile (top position and bottom position) of the filter
+        """
+        return {f"{i} --> {v.well_bro_id}": {"top position": v.screen_top_position, 
+                                "bottom position": v.screen_bottom_position} 
+                                for i, v in self.well_filter.items()}
 
     def __str__(self):
-        for well_number, observation in self.well_observations.items():
+        for well_number, observation in self.well_filter.items():
             print(observation)
 
     def __len__(self):
-        return len(self.well_observations)
+        return len(self.well_filter)
     
 
 
@@ -91,6 +108,17 @@ def parse_gmw_wells(file_path: str) -> Well:
     return _well
 
 def xml_append_well_observations(file_path: str, well: Well) -> Well:
+    """Append a well observation to an existing Well object from an XML file.
+    This function reads an XML file containing well observations and extracts the relevant data
+    Warning: if Well is empty and if it lacks the well number, it will create a new well observation.
+
+    Args:
+        file_path (str): path to the XML file containing well data
+        well (Well): An existing Well object with filter, to which data will be added
+
+    Returns:
+        Well: Well object with filter data added from the XML file
+    """
     # Register namespaces with prefixes
     namespaces = {
         'dsgld': 'http://www.broservices.nl/xsd/dsgld/1.0',
@@ -102,7 +130,7 @@ def xml_append_well_observations(file_path: str, well: Well) -> Well:
     }
 
     # Load and parse the XML file
-    tree = ET.parse(file_path)  # replace with actual file path
+    tree = ET.parse(file_path)
     root = tree.getroot()
 
     # Extract broId and wellNumber
