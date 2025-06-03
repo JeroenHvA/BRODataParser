2b15e5b2a06035d81aafeada7fcf0e404341fd5b
JeroenHvA <165269862+JeroenHvA@users.noreply.github.com>
2025-06-03 12:44:43 +0200

    wellFilter and wellFilterData added to broparser.parser

--- broparser/parser/well_filter.py
+++ broparser/parser/well_filter.py
@@ -2,126 +2,12 @@ from dataclasses import dataclass
 from typing import Dict, List
 from datetime import datetime
 
-@dataclass
-class wellFilterData:
-
-    def __init__(self):
-        self.columns: List = None
-        self.dataset: Dict[datetime, float] = {}
-
-    def add_observation(self, date_time: datetime|str, value: list, 
-                        columns: List = None, return_errors: bool = False) -> None:
-        """Append a new observation to the dataset. \n
-        If the date_time is a string, it will be converted to a datetime object.
-
-        Args:
-            date_time (datetime | str): datetime of the observation
-            value (list): value of the observation, must be a list of values
-            columns (List, optional): set column names. Defaults to None.
-            return_errors (bool, optional): if errors need to be returned, else None. Defaults to False.
-
-        Returns:
-            None or Exception: None or error message if return_errors is True.
-        """
-        if isinstance(date_time, str):
-            date_time = datetime(year = int(date_time[:4]), month= int(date_time[5:7]), 
-                         day = int(date_time[8:10]), hour = int(date_time[11:13]), 
-                         minute = int(date_time[14:16]), second= int(date_time[17:19]))
-        
-        try:
-            self.dataset[date_time] = value
-        except Exception as e:
-            print(f"Error adding observation: {date_time}, reason {e}")
-            if return_errors:
-                return e
-            pass
-        if columns != None:
-            self.add_columns(columns)
-        return None
-
-    def subset_observations(self, start: datetime = None, end: datetime = None) -> Dict[datetime, float]:
-        """Subset the observations based on a start and end datetime.
-
-        Args:
-            start (datetime, optional): Start datetime of dataset. Defaults to None.
-            end (datetime, optional): End datetime dataset. Defaults to None.
-        Returns:
-            Dict: A subset of the dataset containing only the observations within the specified range.
-        
-        """
-        ss = self.dataset
-        self.__any_datacheck("No data in dataset, cannot subset")
-        if start != None:
-            ss = {k: v for (k, v) in ss.items() if k >= start}
-        if end != None:
-            ss = {k: v for (k, v) in ss.items() if k <= end}
-        return ss
-       
-    def add_columns(self, columns: List) -> None:
-        """Adds columns to the dataset. \n
-        You can only append columns if there is data in the dataset
-
-        Args:
-            columns (List): Columns to append to dataset. \n
-            Assert that length of columns is amount of fields in the dataset
-        
-        Returns:
-            None
-        """
-
-        self.__any_datacheck("Couldn't set columns, no data in dataset yet")
-
-        if len(self.dataset[list(self.dataset.keys())[0]]) + 1 == len(columns):
-            self.columns = columns
-        else:
-            print("Columns of different length to dataset")
-    
-    def order(self):
-        """Sort the dataset by datetime.
-
-        Returns:
-            Dict: Sorted dictionary by datetime.
-        """
-        self.dataset = dict(sorted(self.dataset.items(), key = lambda item: item[0]))
-        return self.dataset
-
-    def __str__(self):
-        print_str = ""
-        if not self.columns == None:
-            print_str += " ----- ".join([str(t) for t in self.columns]) + " \n"
-            
-        if self.__any_datacheck(""):
-            if len(self.dataset) > 7:
-                for i in list(self.dataset.keys())[:3]:
-                    print_str += self.__printline(i)
-                print_str += ".... \n"
-                for i in list(self.dataset.keys())[-3:]:
-                    print_str += self.__printline(i)
-            else:
-                for i in self.dataset.keys():
-                    print_str += self.__printline(i)
-
-        return print_str
-                    
-    def __printline(self, i):
-        if self.dataset[i] != None:
-            if len(self.dataset[i]) > 0 or i != None:
-                return f"{i}: {", ".join([str(t) for t in self.dataset[i] if t != None])} \n"
-
-    def __len__(self):
-        if self.dataset != None:
-            return len(self.dataset.keys())
-        else:
-            return 0
-        
-    def __any_datacheck(self, message = ""):
-        if len(self.dataset) > 0:
-            return True
-        else:
-            raise ValueError(message)
+from .dataframe import wellFilterData
 
 class wellFilter:
     def __init__(self, gld_bro_id, well_number, well_bro_id):
+
+        #TODO: link to well object
         self.gld_bro_id = gld_bro_id ## common bro id
         self.well_number = well_number
         self.well_bro_id = well_bro_id ## unique bro id for this well
@@ -131,6 +17,8 @@ class wellFilter:
         self.screen_bottom_position = None
 
         self.dataset = wellFilterData()  # dictionary with 
+        self.start_date = None
+        self.end_date = None
 
     def add_metadata(self, screen_length: float = None, screen_top_position:float = None, screen_bottom_position:float = None):
         """Add metadata to the well observation.
@@ -143,6 +31,43 @@ class wellFilter:
         self.screen_length = screen_length
         self.screen_top_position = screen_top_position
         self.screen_bottom_position = screen_bottom_position
+    
+    def subset_observations(self, start: datetime = None, end: datetime = None) -> Dict[datetime, float]:
+        """Subset the observations based on a start and end datetime.
+
+        Args:
+            start (datetime, optional): Start datetime of dataset. Defaults to None.
+            end (datetime, optional): End datetime dataset. Defaults to None.
+        
+        Returns:
+            Dict: A subset of the dataset containing only the observations within the specified range.
+        """
+        self.dataset.order()
+        return self.dataset.subset_observations(start, end)
+    
+    def get_start_date(self) -> datetime:
+        """Get the start date of the dataset.
+
+        Returns:
+            datetime: Start date of the dataset.
+        """
+        if self.start_date == None:
+            self.start_date = min(self.dataset.dataset.keys())
+        return self.start_date
+    
+    def get_end_date(self) -> datetime:
+        """Get the end date of the dataset.
+
+        Returns:
+            datetime: End date of the dataset.
+        """
+        if self.end_date == None:
+            self.end_date = max(self.dataset.dataset.keys())
+        return self.end_date
+
+    
+    def __repr__(self):
+        return self.dataset.__repr__()
 
     def __str__(self):
         return f"well #{self.well_number} (brocom: {self.well_bro_id}, gldcommon:{self.gld_bro_id}): {len(self.dataset)} measurements"
